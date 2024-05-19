a9d800dc12aab5145736db19a70ceca8268b9685
Yeoh Joer <yeoh.joer@outlook.com>
2024-05-19 20:49:37 +0800

    Add evaluation summary

---
+++ pages/evaluation_summary.py
@@ -0,0 +1,49 @@
+import mlflow
+import streamlit as st
+from crunchy_mining import mlflow_util
+from crunchy_mining.util import summarize_classification
+
+from pages.fragments import plot_best_by_recall_score
+from pages.fragments import plot_worst_by_recall_score
+
+st.set_page_config(layout="wide")
+mlflow.set_tracking_uri("http://localhost:5002")
+
+with st.spinner("Fetching experiment data..."):
+    df = mlflow_util.get_cv_metrics_by_task(task_name="clf")
+
+outputs = summarize_classification(df)
+chart_conf = {"use_container_width": True, "theme": None}
+
+plot_best_by_recall_score(df)
+plot_worst_by_recall_score(df)
+
+st.altair_chart(outputs["auc_chart"], **chart_conf)
+st.dataframe(outputs["auc"], use_container_width=True)
+
+st.altair_chart(outputs["auc_min_chart"], **chart_conf)
+st.dataframe(outputs["auc_min"], use_container_width=True)
+
+st.altair_chart(outputs["fit_time_chart"], **chart_conf)
+st.dataframe(outputs["fit_time"], use_container_width=True)
+
+st.altair_chart(outputs["fit_time_max_chart"], **chart_conf)
+st.dataframe(outputs["fit_time_max"], use_container_width=True)
+
+st.altair_chart(outputs["score_time_chart"], **chart_conf)
+st.dataframe(outputs["score_time"], use_container_width=True)
+
+st.altair_chart(outputs["score_time_max_chart"], **chart_conf)
+st.dataframe(outputs["score_time_max"], use_container_width=True)
+
+st.altair_chart(outputs["fit_memory_chart"], **chart_conf)
+st.dataframe(outputs["fit_memory"], use_container_width=True)
+
+st.altair_chart(outputs["fit_memory_max_chart"], **chart_conf)
+st.dataframe(outputs["fit_memory_max"], use_container_width=True)
+
+st.altair_chart(outputs["score_memory_chart"], **chart_conf)
+st.dataframe(outputs["score_memory"], use_container_width=True)
+
+st.altair_chart(outputs["score_memory_max_chart"], **chart_conf)
+st.dataframe(outputs["score_memory_max"], use_container_width=True)
--- pages/fragments.py
+++ pages/fragments.py
@@ -1,4 +1,5 @@
 import altair as alt
+import numpy as np
 import pandas as pd
 import streamlit as st
 
@@ -24,6 +25,60 @@ def model_selector(model_names):
 
 def create_model_selector():
     return model_selector(model_names)
+
+
+def plot_best_by_recall_score(df: pd.DataFrame):
+    df_recall = df.loc[df.groupby("experiment_id")["recall_1"].idxmax()]
+    df_recall["experiment_id"] = df_recall["experiment_id"].astype(int)
+    df_recall.sort_values(by="experiment_id", inplace=True)
+    df_recall["experiment_idx"] = np.arange(1, len(df_recall) + 1)
+
+    st.altair_chart(
+        alt.Chart(
+            data=df_recall,
+            title=alt.TitleParams(
+                text="Best Models by Recall Score Across Experiments",
+                anchor="start",
+            ),
+        )
+        .mark_bar()
+        .encode(
+            x=alt.X("experiment_idx:N", title="Experiments").sort("x"),
+            y=alt.Y("recall_1:Q", title="Recall Score"),
+            color=alt.Color("parent_run_name:N", title="Models"),
+        ),
+        use_container_width=True,
+        theme=None,
+    )
+
+    st.dataframe(df_recall)
+
+
+def plot_worst_by_recall_score(df: pd.DataFrame):
+    df_recall_min = df.loc[df.groupby("experiment_id")["recall_1"].idxmin()]
+    df_recall_min["experiment_id"] = df_recall_min["experiment_id"].astype(int)
+    df_recall_min.sort_values(by="experiment_id", inplace=True)
+    df_recall_min["experiment_idx"] = np.arange(1, len(df_recall_min) + 1)
+
+    st.altair_chart(
+        alt.Chart(
+            data=df_recall_min,
+            title=alt.TitleParams(
+                text="Worst Models by Recall Score Across Experiments",
+                anchor="start",
+            ),
+        )
+        .mark_bar()
+        .encode(
+            x=alt.X("experiment_idx:N", title="Experiments").sort("x"),
+            y=alt.Y("recall_1:Q", title="Recall Score"),
+            color=alt.Color("parent_run_name:N", title="Models"),
+        ),
+        use_container_width=True,
+        theme=None,
+    )
+
+    st.dataframe(df_recall_min)
 
 
 def plot_recall_score_by_experiments(df_cv_metrics: pd.DataFrame):
